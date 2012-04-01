6cbb476865ca57fa12708285fe1308f838bdc30a
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-04-01 07:54:15 -0700

    generalizrd line clearing/handling

--- parser.rb
+++ parser.rb
@@ -1,8 +1,32 @@
 require "./debug"
 module Parser
   attr_accessor :row, :col, :fg, :bg
+
+  ESC="\e"
+  CODES={"A" => {add: [-1, 0]},
+         "B" => {add: [1, 0]},
+         "C" => {add: [0, 1]},
+         "D" => {add: [0, -1]},
+         "m" => nil,
+         "h" => nil,
+         "J" => nil,
+         "H" => {set: /\e\[(\d*);?(\d*)H/},
+         "K" => {clear_line: /\e\[(\d)*K/}
+  }
+
+  LINEHANDLERS={ 1 => "top_line",
+                23 => "attribute_line",
+                24 => "status_line"}
+
+  TERMWIDTH=80
+  @@line_contents = {}
+  for k,v in LINEHANDLERS
+    @@line_contents[v] = " " * TERMWIDTH
+  end
+
   @row = 1
   @col = 1
+
   def self.position
     "%i, %i" % [@row, @col]
   end
@@ -44,38 +68,23 @@ module Parser
 
   def self.clear_line *args
     Debug.log("clearing line %s", args.join)
-    if @col == 23
+    return unless LINEHANDLERS.include? @row
+    for i in @col..TERMWIDTH
+      @@line_contents[LINEHANDLERS[@row]][i] = " "
     end
     #@col = 1
   end
 
-  def self.increment
-    @col += 1
+  def self.increment n=1
+    @col += n
   end
 
   def self.decrement
     @col -= 1
   end
 
-  ESC="\e"
-  CODES={"A" => {add: [-1, 0]},
-         "B" => {add: [1, 0]},
-         "C" => {add: [0, 1]},
-         "D" => {add: [0, -1]},
-         "m" => nil,
-         "h" => nil,
-         "J" => nil,
-         "H" => {set: /\e\[(\d*);?(\d*)H/},
-         "K" => {clear_line: /\e\[(\d)*K/}
-  }
-
-  LINEHANDLERS={ 1 => :parse_topline,
-                23 => :parse_attribute_line,
-                24 => :parse_status_line}
-
 
   def self.parse str
-    @attributes = " "*160 unless @attributes
     i = 0
     while i < str.length
       while str[i] == ESC
@@ -84,16 +93,30 @@ module Parser
         i += 1
       end
 
-# grab a chunk
+      # grab a chunk
       j = i
       while str[j] != ESC && j < str.length
         j += 1
       end
       j -= 1
 
-# send chunk through the proper handler
+      # send chunk through the proper handler
       if LINEHANDLERS.include? @row
-        self.send(LINEHANDLERS[@row], str[i..j])
+        rowname = LINEHANDLERS[@row]
+        handler = "parse_" + rowname
+        handler = handler.to_sym
+
+        # update line container
+        k = @col - 1
+        str[i..j].each_char do |c|
+          break if c == "\e"
+          @@line_contents[rowname][k] = c unless c == "\r"
+          k += 1
+        end
+        self.send(handler, @@line_contents[rowname])
+
+        # advance i if this chunk was handled
+        increment j - i
         i = j
       end
 
@@ -136,17 +159,11 @@ module Parser
     j - i
   end
 
-  def self.parse_topline str
+  def self.parse_top_line str
     Debug.log("Found message %s", str)
   end
 
   def self.parse_attribute_line str
-    i = @col - 1
-    str.each_char do |c|
-      return if c == "\e"
-      @attributes[i] = c unless c == "\r"
-      i += 1
-    end
     #@attributes = [@attributes[-1..i].to_s, str, @attributes[i + str.length..@attributes.length-1]].join
     Debug.log("Found attribute %s", @attributes)
   end
