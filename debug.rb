5cc59f5ecb141ee95d2bac1651f67a706c257f7f
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-07-21 10:51:50 -0700

    refactor debug module

--- debug.rb
+++ debug.rb
@@ -7,57 +7,69 @@ DEBUG = 4
 EXTRA = 5
 
 module Debug
+  LOGBASENAMES = [ :raw, :term, :debug ]
+  @@log_files = {}
+
   def self.start
     @@debuglevel = ($options["loglevel"] || DEBUG).to_i
-    @@logfile = open("./debug.log", "a")
-    log("Opened %s for writing", @@logfile.path)
-    @@rawfile = open("./raw.log", "a")
-    log("Opened %s for writing", @@rawfile.path)
-    @@termfile = open("./term.log", "a")
-    log("Opened %s for writing", @@termfile.path)
+
+    LOGBASENAMES.each do |base_name|
+      open_log base_name
+    end
   end
 
+  def self.open_log base_name
+    new_file = open("./#{base_name}.log", "w")
+    @@log_files[base_name] = new_file
+  end
+
+  # cleanly close all open log files
   def self.stop
-    @@logfile.close
-    @@rawfile.close
+    for file in @@log_files.values
+      file.close
+    end
   end
 
   def self.print (file, str, *args)
     return if str.strip == ""
     file.printf(str + "\n", *args)
     file.flush
+    str
   end
 
-  def self.print_to_log (level, str, *args)
-    print(@@logfile, str, *args) if @@debuglevel >= level
+  # print to the log if the message is important enough
+  def self.log (level, message, *args)
+    print(@@log_files[:debug], message, *args) if @@debuglevel >= level
   end
 
   def self.raw (str, *args)
+    # TODO: wtf does this do...
     while str.sub!(/([^%])%([^%])/,"\\1%%\\2")
     end
-    print(@@rawfile, str + "\n", *args)
+    print(@@log_files[:raw], str + "\n", *args)
   end
 
   def self.term (str)
-    @@termfile.print(str)   
+    # TODO: why does this not use Debug.print() ?
+    @@log_files[:term].print(str)   
   end
 end
   
 def log str, *args
-  Debug.print_to_log LOG, str, *args
+  Debug.log LOG, str, *args
 end
 
 def err str, *args
-  Debug.print_to_log ERROR, str, *args
+  Debug.log ERROR, str, *args
   exit
 end
 
 def dbg str, *args
-  Debug.print_to_log DEBUG, str, *args
+  Debug.log DEBUG, str, *args
 end
 
 def extra str, *args
-  Debug.print_to_log EXTRA, str, *args
+  Debug.log EXTRA, str, *args
 end
 
 def term str
