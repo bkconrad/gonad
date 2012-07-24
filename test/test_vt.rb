cc9c0181ed9b33e8829e6c1be93fdbeaa65ca9c6
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-07-24 14:16:38 -0700

    initial VT rewrite

--- test/test_vt.rb
+++ test/test_vt.rb
@@ -4,67 +4,129 @@ require 'shoulda'
 
 require './vt'
 
-class VTTest < Test::Unit::TestCase
+class VT102Test < Test::Unit::TestCase
 
   context 'Virtual Terminal (VT)' do
     setup do
-      @vt = VT::VTBuffer.new
+      @stream = VT102::Stream.new
+      @screen = VT102::Screen.new
+      @stream.attach @screen
     end
 
-    should 'initialize the buffer' do
-      assert(@vt.get == [1, 1], 'cursor is at origin')
+    should "parse and print text" do
+      @stream.process 'test'
+      assert @screen.to_s.match /^test/
     end
 
-    should 'parse input' do
-      @vt.parse('test')
-      assert @vt.all.match /^test/
+    should 'perform a line feed on \n' do
+      @stream.process "foo\nbar"
+      assert_equal [2, 7], @screen.position
     end
 
-    should 'set cursor position on \e[H' do
-      @vt.parse "\e[13;37H"
-      assert @vt.get == [13, 37]
+    should 'backspace to the beginning of the line' do
+      @stream.process "foo\n\b\b\b\b"
+      assert_equal [2, 1], @screen.position
     end
 
-    should 'set sgr commands' do
-      @vt.parse "\e[36;1mtest"
-      assert @vt.row_glyphs[0][0].color == :cyan
-      assert @vt.row_glyphs[0][0].style == :bold
+    should 'return cursor to first column' do
+      @stream.process "test\r"
+      assert_equal [1, 1], @screen.position
     end
 
-    should 'reset sgr commands when passed nothing or zero' do
-      @vt.parse "\e[36;1mfoo\e[0mbar"
-      assert @vt.row_glyphs[0][0].color == :cyan
-      assert @vt.row_glyphs[0][0].style == :bold
-
-      assert @vt.row_glyphs[0][4].color == :none
-      assert @vt.row_glyphs[0][4].style == :normal
+    should 'move cursor up' do
+      @stream.process "foo\n\rbar\e[A"
+      assert_equal [1, 4], @screen.position
     end
 
-    should 'not reset sgr when passed \e[m without parameters' do
-      @vt.parse "\e[36;1mfoo\e[mbar"
-      assert @vt.row_glyphs[0][0].color == :cyan
-      assert @vt.row_glyphs[0][0].style == :bold
-
-      assert @vt.row_glyphs[0][4].color == :cyan
-      assert @vt.row_glyphs[0][4].style == :bold
+    should 'move cursor down' do
+      @stream.process "foo\n\rbar\e[B"
+      assert_equal [3, 4], @screen.position
     end
 
-    should 'not change lines on a new line character' do
-      @vt.parse "\n"
-      assert @vt.get == [1, 2]
+    should 'move cursor forward' do
+      @stream.process "foo\n\rbar\e[C"
+      assert_equal [2, 5], @screen.position
     end
 
-    should 'return a specific line of text' do
-      @vt.parse "foo\e[2;1Hbar"
-      assert(@vt.row(1).match(/^bar/))
+    should 'move cursor backward' do
+      @stream.process "foo\n\rbar\e[D"
+      assert_equal [2, 3], @screen.position
     end
 
-    should 'return all text concatenated as a string without ANSI codes' do
-      @vt.parse "foo\e[2;1Hbar"
-      assert_equal VT::VTBuffer::TERMWIDTH * VT::VTBuffer::TERMHEIGHT,
-        @vt.all.length
+    should 'move to a specified position' do
+      @stream.process "\e[13;37H"
+      assert_equal [13, 37], @screen.position
+    end
 
-      refute @vt.all.match(/\e/) 
+    should 'clear to the end of the line' do
+      @stream.process "foo\n\rbar\r\e[K"
+      assert @screen.to_s.match /foo/
+      refute @screen.to_s.match /bar/
+    end
+
+    should 'clear to the beginning of the line' do
+      @stream.process "foo\n\rbar\e[1K"
+      assert @screen.to_s.match /foo/
+      refute @screen.to_s.match /bar/
+    end
+
+    should 'clear to the whole line' do
+      @stream.process "---bar\rfoo\e[2K"
+      refute @screen.to_s.match /foo/
+      refute @screen.to_s.match /bar/
+    end
+
+    should 'clear from the cursor down' do
+      # put one X on a line followed by one Y on the beginning of each line
+      # after that
+      @stream.process "X" + "\n\rY"*(@screen.rows - 1)
+      # move to the second line
+      @stream.process "\e[2;1H"
+      # clear from the cursor to the end of the display
+      @stream.process "\e[J"
+      refute @screen.to_s.match /Y/
+    end
+
+    should 'clear from the cursor up' do
+      # put one X on a line preceeded by one Y on the beginning of each line
+      # after that
+      @stream.process "Y\n\r"*(@screen.rows - 1) + "X"
+      # move to the second to last line
+      @stream.process "\e[#{@screen.rows - 1};1H"
+      # clear from the cursor to the beginning of the display
+      @stream.process "\e[1J"
+      refute @screen.to_s.match /Y/
+    end
+
+    should 'clear the whole display' do
+      # put one X on the beginning of each line
+      @stream.process "X" + "\n\rX"*(@screen.rows - 1)
+      @stream.process "\e[2J"
+      refute @screen.to_s.match /X/
+    end
+
+    should 'delete, shift, and append characters' do
+      @stream.process "foo12345bar"
+      @stream.process "\e[1;4H"
+      @stream.process "\e[5P"
+      assert @screen.to_s.match /foobar/
+      assert_equal @screen.cols, @screen.contents[@screen.row - 1].length
+    end
+
+    should 'set color and style' do
+      @stream.process "\e[36;1m"
+      @stream.process "foo"
+      assert_equal :cyan, @screen.contents[0][0].color
+      assert_equal :bold, @screen.contents[0][0].style
+    end
+
+    should 'reset color and style' do
+      @stream.process "\e[36;1m"
+      @stream.process "foo"
+      @stream.process "\e[m"
+      @stream.process "bar"
+      assert_equal :none, @screen.contents[0][3].color
+      assert_equal :normal, @screen.contents[0][3].style
     end
   end
 end
--- vt.rb
+++ vt.rb
@@ -1,55 +1,312 @@
 require "./glyph"
 require "./debug"
 
-# VT takes nethack output from an Interface and produces a 2D array of Glyph s
-# and allows the Parser to query it using several utility functions
+require './escape'
 
-module VT
+# VT102 takes nethack output from an Interface and produces a 2D array of Glyph
+# s and allows the Parser to query it using several utility functions
+
+module VT102
   
-  # a clearable buffer with individual cursor and glyph state tracking
-  class VTBuffer
-    ESC="\e"
+  BASIC = {
+    BS => :backspace,
+    HT => :tab,
+    LF => :linefeed,
+    VT => :linefeed,
+    FF => :linefeed,
+    CR => :carriage_return,
+    SI => :shift_in,
+    SO => :shift_out
+  }
 
-    # a map of ANSI escape codes to the methods that implement them. values may
-    # be either a symbol, a hash, or nil. if a symbol is found, it is taken as
-    # the method name, if a hash is found, the first (only) key is the method
-    # name, and the value is an array of arguments to be passed. if nil is
-    # found, the escape code is ignored
+  ESCAPE = {
+    IND   => :index,
+    RI    => :reverse_index,
+    NEL   => :linefeed,
+    DECSC => :store_cursor,
+    DECRC => :restore_cursor,
+    RLF   => :reverse_linefeed
+  }
 
-    CODES={"A" => {add: [-1, 0]},
-           "B" => {add: [1, 0]},
-           "C" => {add: [0, 1]},
-           "D" => {add: [0, -1]},
-           "m" => :set_sgr,
-           "h" => nil,
-           "J" => :clear_data,
-           "H" => :set,
-           "K" => :clear_line
-    }
+  SEQUENCE = {
+    CUU     => :cursor_up,
+    CUD     => :cursor_down,
+    CUF     => :cursor_right,
+    CUB     => :cursor_left,
+    CUP     => :cursor_move,
+    HVP     => :cursor_move,
+    EL      => :erase_in_line,
+    ED      => :erase_in_display,
+    DCH     => :delete_characters,
+    IL      => :insert_lines,
+    DL      => :delete_lines,
+    SGR     => :select_graphic_rendition,
+    DECSTBM => :set_margins,
+    IRMI    => :set_insert,
+    IRMR    => :set_replace
+  }
 
-    # regex to match any recognized escape code character
-    ESCAPECODEREGEX = Regexp.new("[#{CODES.keys.join}]")
+  class Screen
+    attr_accessor :contents, :row, :col, :rows, :cols
+    def initialize rows = 24, cols = 80
+      @contents = []
 
-    # regex to match parameters inside of an escape
-    ESCAPEPARAMETERREGEX = /(\d+);?/
-
-    # regex to match an escape code which begins the test string
-    # matches the code specifier
-    ESCAPEREGEX = /^\x1B\[[\d;]*([\w])/
-
-    TERMWIDTH=80
-    TERMHEIGHT=24
-
-    def initialize
-      @line_contents = []
-      for i in 0...TERMHEIGHT
-        @line_contents[i] = [].fill(0, TERMWIDTH) { Glyph.new }
-      end
-
+      @rows = rows
+      @cols = cols
       @row = 1
       @col = 1
       @fg = Glyph::COLOR[:none]
       @style = Glyph::STYLE[:normal]
+      @tab_stops = []
+
+      for i in 0...@rows
+        @contents[i] = [].fill(0, @cols) { Glyph.new }
+      end
+    end
+
+    def position
+      [ @row, @col ]
+    end
+
+    def print char
+      unless (1..@rows).cover?(@row) && (1..@cols).cover?(@col)
+        raise Exception, "#{@row}, #{@col} is out of range"
+      end
+
+      # translate from terminal coords to array coords
+      @contents[@row - 1][@col - 1].char = char
+      @contents[@row - 1][@col - 1].color = @fg
+      @contents[@row - 1][@col - 1].style = @style
+
+      @col += 1
+      if @col > @cols
+        linefeed
+      end
+    end
+
+    def linefeed
+      if @row + 1 > @rows
+        @contents.shift
+        @contents.push([].fill(0...@cols) { Glyph.new })
+      else
+        @row += 1
+      end
+    end
+
+    def backspace
+      @col = [1, @col - 1].max
+    end
+
+    def tab
+      @col = next_tab
+    end
+
+    # return next tab stop or the right-most column's column mumber
+
+    def next_tab
+      @tab_stops.sort.each do |stop|
+        if @col < stop
+          return stop
+        end
+      end
+
+      return @cols
+    end
+
+    def carriage_return
+      @col = 1
+    end
+
+    # TODO: G0 character set
+    def shift_in
+    end
+
+    # TODO: G1 character set
+    def shift_out
+    end
+
+    # sequence handlers
+
+    def cursor_up
+      @row = [1, @row - 1].max
+    end 
+
+    def cursor_down
+      @row = [@rows, @row + 1].min
+    end 
+
+    def cursor_left
+      @col = [1, @col - 1].max
+    end 
+
+    def cursor_right
+      @col = [@cols, @col + 1].min
+    end 
+
+    def cursor_move row = 1, col = 1
+      @row = row
+      @col = col
+    end
+
+    def erase_in_line type = 0
+      case type.to_i
+      when 0
+        # cursor to end of line, including cursor
+        @contents[@row - 1].fill(@col - 1) { Glyph.new }
+      when 1
+        # beginning of line to cursor, including cursor
+        @contents[@row - 1].fill(0...@col) { Glyph.new }
+      when 2
+        @contents[@row - 1].fill(0...@cols) { Glyph.new }
+      end
+    end
+
+    def erase_in_display type = 0
+      case type.to_i
+      when 0
+        # cursor to end of display, including cursor
+        # clear current line
+        @contents[@row - 1].fill(@col - 1) { Glyph.new }
+        # clear succeeding lines
+        for i in @row...@rows
+          @contents[i].fill(0...@cols) { Glyph.new }
+        end
+      when 1
+        # beginning of display to cursor, including cursor
+        # clear current line
+        @contents[@row - 1].fill(0...@col) { Glyph.new }
+        # clear succeeding lines
+        for i in 0...@row
+          @contents[i].fill(0...@cols) { Glyph.new }
+        end
+      when 2
+        clear_data
+      end
+    end
+
+    def delete_characters n = 1
+      n.times do 
+        @contents[@row - 1].slice! @col - 1
+        @contents[@row - 1].push Glyph.new
+      end
+    end
+
+    # sets the current SGR (Select Graphic Rendition) properties such as
+    # background color, bold/normal, foreground color, etc
+
+    def select_graphic_rendition *args
+      if args === []
+        extra "resetting style and color"
+        @style = Glyph::STYLE[:normal]
+        @fg = Glyph::COLOR[:none]
+      end
+      for arg in args
+        if arg.to_i === 0
+          extra "resetting style and color"
+          @style = Glyph::STYLE[:normal]
+          @fg = Glyph::COLOR[:none]
+        elsif (Glyph::STYLE[:bold]..Glyph::STYLE[:normal]).cover? arg.to_i
+          extra "setting style to #{arg}"
+          @style = arg
+        else
+          extra "setting color to #{arg}"
+          @fg = arg
+        end
+      end
+    end
+
+    def clear_data *args
+      extra("clearing data with %s", args)
+      for i in 0...@rows
+        @contents[i].fill 0...@cols do Glyph.new end
+      end
+    end
+
+    def to_s
+      result = ""
+      @contents.each { |row| result += row.join + "\n"}
+      result
+    end
+  end
+
+  # State machine to parse text and maintain a terminal state based on the ANSI
+  # control codes found
+  class Stream
+    def initialize
+      @listeners = []
+      @state = :stream
+      @parameters = []
+      @current_parameter = ""
+    end
+
+    # process a string as terminal output
+
+    def process str
+      str.each_char do |char|
+        # route each character to its state handler
+        send(@state, char)
+      end
+    end
+
+    # attach a screen to this stream
+
+    def attach listener
+      @listeners.push listener
+    end
+
+    private
+
+    def notify method, *args
+      @listeners.each do |listener|
+        listener.send method, *args
+      end
+    end
+
+    # stream mode handler
+
+    def stream char
+      if BASIC[char] != nil
+        notify BASIC[char]
+      elsif char === ESC
+        @state = :escape
+      else BASIC[char] == nil
+        notify :print, char
+      end
+    end
+
+    # escape mode handler
+
+    def escape char
+      case char
+      when '['
+        @state = :escape_parameters
+      end
+    end
+
+    # escape parameter mode handler
+
+    def escape_parameters char
+      /[^0-9;]/.match char
+      case char
+      when $&
+        # if the character is not a parameter or separator, it must be a
+        # sequence delimiter
+
+        unless @current_parameter == ""
+          @parameters.push @current_parameter.to_i
+        end
+
+        notify SEQUENCE[char], *@parameters
+
+        @parameters = []
+        @current_parameter = ""
+        @state = :stream
+      when ';'
+        @parameters.push @current_parameter.to_i
+        @current_parameter = ""
+      else
+        @current_parameter += char
+      end
     end
 
     def position
@@ -69,184 +326,16 @@ module VT
       @col = col.to_i unless col == -1
     end
 
-    # sets the current SGR (Select Graphic Rendition) properties such as
-    # background color, bold/normal, foreground color, etc
-
-    def set_sgr *args
-      if args === []
-        extra "resetting style and color"
-        @style = Glyph::STYLE[:normal]
-        @fg = Glyph::COLOR[:none]
-      end
-      for arg in args
-        if arg === ''
-          next
-        end
-
-        if arg.to_i === 0
-          extra "resetting style and color"
-          @style = Glyph::STYLE[:normal]
-          @fg = Glyph::COLOR[:none]
-        elsif (Glyph::STYLE[:bold]..Glyph::STYLE[:normal]).cover? arg.to_i
-          extra "setting style to #{arg}"
-          @style = arg
-        else
-          extra "setting color to #{arg}"
-          @fg = arg
-        end
-      end
-    end
-
     def add row, col
       @row += row.to_i
       @col += col.to_i
     end
 
-    def parse_escape str
-
-      escape_sequence = ESCAPEREGEX.match(str)
-
-      unless escape_sequence
-        extra "parse_escape called on string not starting with ESC\n%s", str
-        return str
-      end
-
-      action = escape_sequence.captures[0]
-      method_specifier = CODES[action]
-      method_args = nil
-
-      case method_specifier
-      when Symbol
-        method = method_specifier
-      when Hash
-        method = method_specifier.keys[0]
-        method_args = method_specifier.values[0]
-      when nil
-        # TODO: handle the do-nothing case
-      end
-
-      # if nil, we search for the parameters using the regex and pass them as an
-      # array
-      if method_args === nil
-        method_args = []
-
-        # start after the "\e["
-        i = 2
-        while parameter_match = escape_sequence.to_s[i..-1].match(ESCAPEPARAMETERREGEX)
-          method_args.push parameter_match.captures[0]
-          i += parameter_match.captures[0].length
-        end
-      end
-
-      self.send(method, *method_args)
-      extra("Set cursor position to %s", position)
-
-      return str[(escape_sequence.to_s.length)..-1]
-    end
-
-    def parse_text str
-      return "" if str == nil || str.length < 1 
-      i = 0
-      while str[i] != ESC && i < str.length
-        write_char str[i]
-        i += 1
-      end
-      extra("Write to line\n%s", str[0...i].inspect)
-
-      # TODO: check that this does what it's meant to...
-      return str[i..-1]
-    end
-
-    def clear_data *args
-      extra("clearing data with %s", args)
-      for i in 0...TERMHEIGHT
-        @line_contents[i].each_index do |j|
-          @line_contents[i][j] = Glyph.new
-        end
-      end
-    end
-
     def clear_line *args
       extra("clearing line " + @row.to_s + " with %s", args.join)
       for i in (@col-1)...TERMWIDTH
-        @line_contents[@row-1][i] = Glyph.new
+        @contents[@row-1][i] = Glyph.new
       end
-    end
-
-    def increment n=1
-      @col += n.to_i
-      if @col >= TERMWIDTH
-        @col = 1
-        @row += 1
-      end
-    end
-
-    def decrement
-      @col -= 1
-    end
-
-    def parse str
-      if str.length > 0
-        extra("BEGIN PARSE\n%s",str.inspect)
-        while str.length > 0
-          str = parse_escape str
-          extra("After parse_escape\n%s", str.inspect)
-          str = parse_text str
-          extra("After parse_text\n%s", str.inspect)
-        end
-        extra("END PARSE FRAME\n")
-      end
-    end
-
-    def write_char char
-      unless (1..TERMHEIGHT).cover?(@row) && (1..TERMWIDTH).cover?(@col)
-        err("Position %s,%s out of range", @row, @col)
-        return
-      end
-
-      if char == "\b"
-        decrement
-      elsif char != "\r"
-        @line_contents[@row-1][@col-1].char = char
-        @line_contents[@row-1][@col-1].color = @fg
-        @line_contents[@row-1][@col-1].style = @style
-        increment
-      end
-    end
-
-    def row_glyphs
-      @line_contents
-    end
-
-    def dump
-      # clear screen
-      str = "\e[H\e[2J\e[H"
-      @line_contents.each_index do |i|
-        str += "\e[" + (i+1).to_s + ";1H"
-        @line_contents[i].each do |glyph|
-          str += glyph.to_ansi
-        end
-      end
-      str += "\e[" + @row.to_s + ";" + @col.to_s + "H"
-      str
-    end
-
-    # returns the full contents of the VTBuffer as a string, or nil if it is
-    # empty
-
-    def all
-      if @line_contents != nil
-        result = @line_contents.map do |line|
-          next line.join
-        end
-        return result.join
-      end
-    end
-
-    # returns the contents of the 0-indexed row number as a string
-
-    def row row_index
-      return @line_contents[row_index].join if @line_contents != nil
     end
   end
 end
